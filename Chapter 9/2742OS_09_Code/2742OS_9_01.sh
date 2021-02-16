5b4be6928201f788b266fb6378543d430101c351
JkTo7 <jtodd1158@gmail.com>
2021-02-16 09:09:13 -0600

    Update 2742OS_9_01.sh

--- Chapter 9/2742OS_09_Code/2742OS_9_01.sh
+++ Chapter 9/2742OS_09_Code/2742OS_9_01.sh
@@ -12,11 +12,11 @@ EOF
 
 mysql -u $USER -p$PASS students <<EOF 2> /dev/null
 CREATE TABLE students(
-id int,
-name varchar(100),
-mark int,
-dept varchar(4)
-);
+  id int,
+  name varchar(100),
+  mark int,
+  dept varchar(4)
+  );
 EOF
 
 [ $? -eq 0 ] && echo Created table students || echo Table students already exist 
